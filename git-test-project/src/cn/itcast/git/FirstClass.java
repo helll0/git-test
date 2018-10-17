1a389dc7d16db358ca140cea91e84fda57aa28a7
zhangsan <zhangsan@itcast.cn>
2018-10-17 19:15:34 +0800

    修改test

--- git-test-project/src/cn/itcast/git/FirstClass.java
+++ git-test-project/src/cn/itcast/git/FirstClass.java
@@ -7,9 +7,11 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+        //teste
     }
 
     public void test() {
         int i =2;
+        System.out.println(i);
     }
 }
